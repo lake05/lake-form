3c7a2be18181903493f9a0afac21aa730a33c017
lake05 <unknown@.com>
2023-04-13 14:17:22 +0800

    ci: added lint-staged eslint and commitlint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,17 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
     'prettier',
   ],
   overrides: [],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['vue'],
+  plugins: ['@typescript-eslint', 'vue'],
   rules: {},
 }
